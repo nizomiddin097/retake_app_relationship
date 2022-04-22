300b2765b3d6c20c2eff025b658cf4669a844067
Nizomiddin <nizomiddinmirzanazarov@gmail.com>
2022-04-22 00:52:07 -0700

    secondCommit

--- src/main/java/uz/pdp/retake_app_relationship/controller/StudentController.java
+++ src/main/java/uz/pdp/retake_app_relationship/controller/StudentController.java
@@ -9,6 +9,7 @@ import uz.pdp.retake_app_relationship.entity.Student;
 import uz.pdp.retake_app_relationship.repository.StudentRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/student")
@@ -31,6 +32,34 @@ public class StudentController {
 
      return studentPage;
  }
+ @PostMapping("/add")
+    public String addStudent(@RequestBody Student student){
+        Student student1 = new Student();
+        student1.setFirstName(student.getFirstName());
+        student1.setLastName(student.getLastName());
+        studentRepository.save(student1);
+        return "Student added";
+ }
+
+    @PutMapping("/add")
+    public String editStudent(@PathVariable Integer id,@RequestBody Student student){
+        Optional<Student> optionalStudent = studentRepository.findById(id);
+        if (optionalStudent.isPresent()) {
+            Student student1 = optionalStudent.get();
+            student1.setFirstName(student.getFirstName());
+            student1.setLastName(student.getLastName());
+            studentRepository.save(student1);
+            return "Student edited";
+        }
+        return "Student not found";
+    }
+
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteStudent(@PathVariable Integer id) {
+        studentRepository.deleteById(id);
+        return " Student deleted";
+    }
 
 
 }
